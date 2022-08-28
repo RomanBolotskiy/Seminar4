37e8c5c5045ed7252f61a79da45f7612fa6729c0
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-28 21:01:21 +0300

    Факториал числа N

---
+++ Task28/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+int MultiDigital(int N)
+{
+    int a = 1;
+
+    for (int i = 2; i <= N; i++)
+{
+    a = a*i;
+}
+
+    return a;
+}
+
+Console.Write (MultiDigital(num));
