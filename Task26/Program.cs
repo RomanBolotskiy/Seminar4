fc5d0694d8988bec6d66ee8dad142c02f310a182
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-28 20:41:01 +0300

    Количество цифр в числе

---
+++ Task26/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+
+int num = int.Parse(Console.ReadLine());
+
+int DigitCount (int a)
+{
+    int count = 0;
+    while (a > 0)
+    {
+        count++;
+        a /= 10;
+    }
+    return count;
+}
+
+Console.Write (DigitCount(num));
