0a22a22d6c893922192af1f80e5b1fc27d81a86a
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-29 21:21:52 +0300

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+*/
+
+  Console.Write("Введите число A: ");
+  int numA = int.Parse(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numB = int.Parse(Console.ReadLine());
+
+int Exponent(int numA, int numB)
+{
+  int result = 1;
+  for(int i=1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+    return result;
+}
+
+  
+  int exponent = Exponent(numA, numB);
+  Console.WriteLine("Ответ: " + exponent);
---
+++ Task30/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void RandArray()
+{
+    int[] arr = new int [8];
+
+    for(int i = 0; i<0; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    for(int i = 0; i<8; i++)
+    {
+        Console.Write("{0} ", arr[i]);
+    }
+}
+
+RandArray();
+
