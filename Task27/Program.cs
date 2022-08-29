c46c6a584263b66707e36d26c872b75b6af4188a
RomanBolotskiy <romanbolotskiy@gmail.com>
2022-08-29 21:34:25 +0300

    Задача 27, сумма цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+*/
+
+Console.Write("Введите число A: ");
+int numA = int.Parse(Console.ReadLine());
+
+  int SumNum(int numA)
+  {
+    
+    int count = Convert.ToString(numA).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      advance = numA - numA % 10;
+      result = result + (numA - advance);
+      numA = numA / 10;
+    }
+   return result;
+  }
+
+int sumNum = SumNum(numA);
+Console.WriteLine("Сумма цифр: " + sumNum);
